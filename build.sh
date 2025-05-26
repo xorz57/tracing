11a66da98ba2586c6cb1b7902b14b4d1a5081abf
Georgios Fotopoulos <xorz57@gmail.com>
2025-05-26 19:32:53 +0200

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+BUILD_TYPE=${1:-Release}
+BUILD_DIR="cmake-build-${BUILD_TYPE,,}"
+
+cmake -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S .
+cmake --build "$BUILD_DIR" --config "$BUILD_TYPE" -- -j"$(nproc)"
+ctest --test-dir "$BUILD_DIR" --build-config "$BUILD_TYPE" --output-on-failure
+
